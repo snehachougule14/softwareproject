4753f1b4e42eaf2c3123cb733a6b1e45e9c0dd87
snehachougule14 <your_email@youremail.com>
2023-10-13 15:59:49 +0530

    variables js code

---
+++ variables.js
@@ -0,0 +1,23 @@
+// variable: are the container to store data(data type) 
+ //let/var/const default var(keywords) folowed by VN  
+ //VN : rule >>> space not allowed,  
+ //first ltr of variable should always alphabets, 
+ //special charcters not allowed(except $_) 
+  
+  
+  
+  
+ let fName; //declaration of variable //undefined 
+  fName = 'pooja'; //defination (syntax variableName = value/data) //difference = == === 
+  
+  let myFirstName = 'poonam'; //declr + def 
+  
+  var mySName='patil'; 
+  
+  var myName; 
+  myName = 'om'; 
+  myName = 'shweta'; 
+  
+  const cityName ='pune'; //const keyword intialization of value is must 
+ //  cityName = 'mumbai' 
+ console.log('cityName',cityName);
