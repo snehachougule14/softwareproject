d8050abaf0cef9ac991d4e2a97bf959e7547d950
snehachougule14 <your_email@youremail.com>
2023-10-15 18:35:11 +0530

    my datatype practice 1

---
+++ data.js
@@ -0,0 +1,42 @@
+let state ="maharashtra";
+console.log('state is',state);
+
+var price = 14;
+console.log("price is :",price);
+  
+var isMyNo =true; // or false 
+console.log("my number is:",isMyNo);
+
+console.log('type of variable:', typeof(isMyNo));
+
+var myHobby = null;
+console.log(myHobby);
+console.log(typeof(myHobby));
+
+let s = 211522245222;
+ console.log("s=", s);
+
+ let b = "33333333333333";
+ console.log(BigInt(b));  
+
+ let a = 1111111111111n
+ console.log(a);
+
+ // array//
+
+ let myName ="sneha live in pune" 
+ console.log(myName);
+
+ let fruits = [];
+
+ fruits = ['apple','mango','banana','pineapple','chiku'];
+
+ console.log( fruits);
+ console.log(typeof(fruits));
+
+ console.log(fruits[3]);
+ console.log(fruits[0]);
+ console.log(fruits[8]);
+ console.log(fruits.length);
+console.log(fruits[fruits.length -1]);
+
