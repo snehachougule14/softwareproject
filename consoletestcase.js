26880cbae575ace11a0ec9802effe314e2968116
snehachougule14 <your_email@youremail.com>
2023-10-20 18:15:21 +0530

    console use

---
+++ consoletestcase.js
@@ -0,0 +1,21 @@
+
+
+
+console.log();  //inbuild fuction used to check values of variables etc
+
+let a = 10;
+console.log(a);  //10
+
+console.log('a value>>',a);
+console.log('a value>>'+a);  //+ concatination operator 2. arithmatic operator
+
+let b = 20;
+console.log(a + b); //30
+
+let c = "30";
+
+console.log(b + c); //2030
+console.log(a + b + c);//3030
+console.log(c + b + a); //302010
+
+
