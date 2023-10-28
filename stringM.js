b2e7eb513d8eaf568cf4b36a6d638f71416b8e73
snehachougule14 <your_email@youremail.com>
2023-10-28 11:22:49 +0530

    string methods

--- stringM.js
+++ stringM.js
@@ -35,11 +35,51 @@ console.log(text.replace( /uSer live In Mumbai , i like Mumbai/i, "poonam"));
 
 console.log(text.replace('I like','lllll'));
 
-function replaceFirstOccurrence(str, find, replace)
+//function replaceFirstOccurrence(str, find, replace)
 
-{ return str.replace(new RegExp(find), replace); }
- var originalString = "This is a test. This is a test. This is a test."; 
- var searchString = "This is a test.";
-  var replacementString = "That was a test."; 
-  var resultString = replaceFirstOccurrence(originalString, searchString, replacementString); 
-console.log(resultString);
+//{ return str.replace(new RegExp(find), replace); }
+// var originalString = "This is a test. This is a test. This is a test."; 
+ //var searchString = "This is a test.";
+  //var replacementString = "That was a test."; 
+ // var resultString = replaceFirstOccurrence(originalString, searchString, replacementString); 
+//console.log(resultString);
+
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+
+//concat()
+
+let firstName = 'poonam';
+let lastName = 'patil';
+let no =60600;
+// let fullName = firstName + " " + lastName;
+let fullName = firstName.concat(' ',no,' ', lastName , ' ',40000);
+
+console.log(fullName);
+
+console.log('lwr->',text.toLowerCase(), 'upr->',text.toUpperCase());
+
+//The trim() method removes whitespace from both sides of a string:
+let data = '         kjkjkj              ';
+console.log(data.trim());
+
+var string = "    This    should  become   something          else   too . ";
+stringData = string.trim().replace(/\s+/g, " ");
+console.log(stringData);
+
+//toString()
+let num = 65656565;
+let data1 = num.toString();
+console.log(data1);
+
+
+
+let text1 = "HELLO WORLD";
+let char = text1.charAt(0);
+console.log(char);
+
+//split()
+let date = "04-april-1992";
+let newDate = date.split('-')  //it splits out data and will return array.
+console.log(newDate);
+console.log(newDate[2]);
