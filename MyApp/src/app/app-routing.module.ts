88321bccd8392b24e506b797721b73cd410fa76b
snehachougule14 <your_email@youremail.com>
2024-01-05 18:06:30 +0530

    signup component

--- MyApp/src/app/app-routing.module.ts
+++ MyApp/src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
 {
   path: '', component :HomeComponent
 },
 {
-  path:'login', component :LoginComponent
+  path:"login", component :LoginComponent
+},
+{
+  path:'signUp', component :SignUpComponent
 }
 
 ];
